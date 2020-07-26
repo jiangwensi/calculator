ce48123cb08f0486c8efe40ce63c5b482e671984
JIANG WENSI <jiangwensi@hotmail.com>
2020-07-26 21:31:53 +0800

    update

--- src/component/Keypad.js
+++ src/component/Keypad.js
@@ -1,75 +1,80 @@
 import React from 'react';
 
 const KeyPad = ({ onClickKey }) => {
-	const keyClassName = 'col-3 py-3 h1 border rounded my-0 bg-light';
-	const keyClassNameWhite = 'col-3 py-3 h1 border rounded my-0 bg-white';
+	const firstColumn = ' offset-sm-2 ';
+	const column = ' h1 col-3 col-sm-2 border rounded my-0 py-3';
+	const white = ' bg-white ';
+	const light = ' bg-light ';
+	const keyClassNameFirstCol = 'col-3 col-sm-2 offset-sm-2 py-3 h1 border rounded my-0 bg-light';
+	const keyClassName = 'col-3 col-sm-2 py-3 h1 border rounded my-0 bg-light';
+	const keyClassNameWhite = 'col-3 col-sm-2 py-3 h1 border rounded my-0 bg-white';
 
 	return (
 		<div className='container'>
 			<div className='row text-center'>
-				<button className={keyClassName} onClick={(e) => onClickKey('C')}>
+				<button className={firstColumn + column + light} onClick={(e) => onClickKey('C')}>
 					C
 				</button>
-				<button className={keyClassName} onClick={(e) => onClickKey('+/-')}>
+				<button className={column + light} onClick={(e) => onClickKey('+/-')}>
 					+/-
 				</button>
-				<button className={keyClassName} onClick={(e) => onClickKey('%')}>
+				<button className={column + light} onClick={(e) => onClickKey('%')}>
 					%
 				</button>
-				<button className={keyClassName} onClick={(e) => onClickKey('/')}>
+				<button className={column + light} onClick={(e) => onClickKey('/')}>
 					/
 				</button>
 			</div>
 			<div className='row text-center'>
-				<button className={keyClassNameWhite} onClick={(e) => onClickKey(7)}>
+				<button className={firstColumn + column + white} onClick={(e) => onClickKey(7)}>
 					7
 				</button>
-				<button className={keyClassNameWhite} onClick={(e) => onClickKey(8)}>
+				<button className={column + white} onClick={(e) => onClickKey(8)}>
 					8
 				</button>
-				<button className={keyClassNameWhite} onClick={(e) => onClickKey(9)}>
+				<button className={column + white} onClick={(e) => onClickKey(9)}>
 					9
 				</button>
-				<button className={keyClassName} onClick={(e) => onClickKey('x')}>
+				<button className={column + light} onClick={(e) => onClickKey('x')}>
 					x
 				</button>
 			</div>
 			<div className='row text-center'>
-				<button className={keyClassNameWhite} onClick={(e) => onClickKey(4)}>
+				<button className={firstColumn + column + white} onClick={(e) => onClickKey(4)}>
 					4
 				</button>
-				<button className={keyClassNameWhite} onClick={(e) => onClickKey(5)}>
+				<button className={column + white} onClick={(e) => onClickKey(5)}>
 					5
 				</button>
-				<button className={keyClassNameWhite} onClick={(e) => onClickKey(6)}>
+				<button className={column + white} onClick={(e) => onClickKey(6)}>
 					6
 				</button>
-				<button className={keyClassName} onClick={(e) => onClickKey('-')}>
+				<button className={column + light} onClick={(e) => onClickKey('-')}>
 					-
 				</button>
 			</div>
 			<div className='row text-center'>
-				<button className={keyClassNameWhite} onClick={(e) => onClickKey(1)}>
+				<button className={firstColumn + column + white} onClick={(e) => onClickKey(1)}>
 					1
 				</button>
-				<button className={keyClassNameWhite} onClick={(e) => onClickKey(2)}>
+				<button className={column + white} onClick={(e) => onClickKey(2)}>
 					2
 				</button>
-				<button className={keyClassNameWhite} onClick={(e) => onClickKey(3)}>
+				<button className={column + white} onClick={(e) => onClickKey(3)}>
 					3
 				</button>
-				<button className={keyClassName} onClick={(e) => onClickKey('+')}>
+				<button className={column + light} onClick={(e) => onClickKey('+')}>
 					+
 				</button>
 			</div>
 			<div className='row text-center'>
-				<button className={`${keyClassNameWhite} col-6`} onClick={(e) => onClickKey(0)}>
+				<button className={`${firstColumn + column + white} col-6 col-sm-4`} onClick={(e) => onClickKey(0)}>
 					0
 				</button>
-				<button className={keyClassNameWhite} onClick={(e) => onClickKey('.')}>
+				<button className={column + white} onClick={(e) => onClickKey('.')}>
 					.
 				</button>
-				<button className={keyClassName} onClick={(e) => onClickKey('=')}>
+				<button className={column + light} onClick={(e) => onClickKey('=')}>
 					=
 				</button>
 			</div>
--- src/component/Result.js
+++ src/component/Result.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 const Result = ({ result, history }) => {
 	return (
-		<div className='border rounded'>
-			<div className='h1 text-right text-break mt-2 mb-0' style={{ height: '5rem', lineHeight: '2.5rem' }}>
-				{history}
+		<div className='container'>
+			<div className='row'>
+				<div className='border rounded offset-sm-2 col-sm-8 '>
+					<div
+						className='h1 text-right text-break mt-2 mb-0'
+						style={{ height: '5rem', lineHeight: '2.5rem' }}>
+						{history}
+					</div>
+					<div className='display-1 text-right my-0 py-0'>{result}</div>
+				</div>
 			</div>
-			<div className='display-1 text-right my-0 py-0'>{result}</div>
 		</div>
 	);
 };
